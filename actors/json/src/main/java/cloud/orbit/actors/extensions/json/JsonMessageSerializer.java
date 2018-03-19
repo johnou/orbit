49a820f329dd0a8f409e5a6e62dec4067dadc956
Johno Crawford <johno.crawford@gmail.com>
2018-03-19 10:24:20 +0100

    Jackson 2.9.4.

--- actors/json/src/main/java/cloud/orbit/actors/extensions/json/JsonMessageSerializer.java
+++ actors/json/src/main/java/cloud/orbit/actors/extensions/json/JsonMessageSerializer.java
@@ -45,12 +45,14 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.core.util.JsonParserSequence;
 import com.fasterxml.jackson.databind.BeanProperty;
+import com.fasterxml.jackson.databind.DatabindContext;
 import com.fasterxml.jackson.databind.DeserializationConfig;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationConfig;
+import com.fasterxml.jackson.databind.annotation.NoClass;
 import com.fasterxml.jackson.databind.cfg.MapperConfig;
 import com.fasterxml.jackson.databind.jsontype.NamedType;
 import com.fasterxml.jackson.databind.jsontype.TypeDeserializer;
@@ -254,12 +256,32 @@ class ClassIdTypeResolverBuilder extends ObjectMapper.DefaultTypeResolverBuilder
 
         TypeIdResolver idRes = idResolver(config, baseType, subtypes, false, true);
 
+        JavaType defaultImpl;
+
+        if (_defaultImpl == null) {
+            defaultImpl = null;
+        } else {
+            // 20-Mar-2016, tatu: It is important to do specialization go through
+            //   TypeFactory to ensure proper resolution; with 2.7 and before, direct
+            //   call to JavaType was used, but that cannot work reliably with 2.7
+            // 20-Mar-2016, tatu: Can finally add a check for type compatibility BUT
+            //   if so, need to add explicit checks for marker types. Not ideal, but
+            //   seems like a reasonable compromise.
+            if ((_defaultImpl == Void.class)
+                    || (_defaultImpl == NoClass.class)) {
+                defaultImpl = config.getTypeFactory().constructType(_defaultImpl);
+            } else {
+                defaultImpl = config.getTypeFactory()
+                        .constructSpecializedType(baseType, _defaultImpl);
+            }
+        }
+
         // First, method for converting type info to type id:
         switch (_includeAs)
         {
             case PROPERTY:
             case EXISTING_PROPERTY: // as per [#528] same class as PROPERTY
-                return new ClassIdAsPropertyTypeDeserializer(baseType, _typeProperty, _typeIdVisible, _defaultImpl, _includeAs, idRes);
+                return new ClassIdAsPropertyTypeDeserializer(baseType, _typeProperty, _typeIdVisible, defaultImpl, _includeAs, idRes);
         }
         throw new IllegalStateException("Do not know how to construct standard type serializer for inclusion type: " + _includeAs);
 
@@ -328,10 +350,10 @@ class ClassIdResolver extends ClassNameIdResolver
 
 
     @Override
-    protected JavaType _typeFromId(final String id, final TypeFactory typeFactory)
+    protected JavaType _typeFromId(String id, DatabindContext ctxt)
     {
         Class<?> cls = DefaultClassDictionary.get().getClassById(Integer.parseInt(id), true);
-        return typeFactory.constructSpecializedType(_baseType, cls);
+        return ctxt.getTypeFactory().constructSpecializedType(_baseType, cls);
     }
 }
 
@@ -342,7 +364,7 @@ class ClassIdAsPropertyTypeDeserializer extends AsPropertyTypeDeserializer
         super(src, property);
     }
 
-    public ClassIdAsPropertyTypeDeserializer(final JavaType baseType, final String _typeProperty, final boolean _typeIdVisible, final Class<?> _defaultImpl, final JsonTypeInfo.As _includeAs, final TypeIdResolver idRes)
+    public ClassIdAsPropertyTypeDeserializer(final JavaType baseType, final String _typeProperty, final boolean _typeIdVisible, final JavaType _defaultImpl, final JsonTypeInfo.As _includeAs, final TypeIdResolver idRes)
     {
         super(baseType, idRes, _typeProperty, _typeIdVisible, _defaultImpl, _includeAs);
     }
